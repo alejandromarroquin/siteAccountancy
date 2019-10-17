e09ffaee4b7120651f0e4e4f2668d630761d7cd6
Alejandro Marroquin Cruz <alejandro.marroquin@dyctech.com>
2019-10-17 09:21:57 -0500

    Identación vista create company

--- resources/views/users/company/create.blade.php
+++ resources/views/users/company/create.blade.php
@@ -88,74 +88,73 @@
             <div class="card-header">{{ __('Datos generales') }}</div>
             <div class="card" style="width: 17rem; margin-top:2em;">
               <div class="col">
-              <label class="col-form-label-sm"><strong>Utilizar datos fiscales: </strong></label>
-              <label class="switch float-right">
-              <input class="publish0" type="checkbox" name="checkcomplete" onclick="completeGeneralInformation();">
-              <span class="slider round"></span>
-            </label>
+                <label class="col-form-label-sm"><strong>Utilizar datos fiscales: </strong></label>
+                <label class="switch float-right">
+                  <input class="publish0" type="checkbox" name="checkcomplete" onclick="completeGeneralInformation();">
+                  <span class="slider round"></span>
+                </label>
+              </div>
             </div>
-            </div>
-
             <div id="generalinf">
-            <div class="row">
-              <div class="col">
-                <label>Télefono de oficina:</label>
-                <input type="text" class="form-control" id="phoneffice" name="phoneffice">
+              <div class="row">
+                <div class="col">
+                  <label>Télefono de oficina:</label>
+                  <input type="text" class="form-control" id="phoneffice" name="phoneffice">
+                </div>
+                <div class="col">
+                  <label>Extención:</label>
+                  <input type="text" class="form-control" id="extension" name="extension">
+                </div>
+                <div class="col">
+                  <label>Celular:</label>
+                  <input type="text" class="form-control" id="cellphone" name="cellphone">
+                </div>
               </div>
-              <div class="col">
-                <label>Extención:</label>
-                <input type="text" class="form-control" id="extension" name="extension">
+              <div class="row">
+                <div class="col">
+                  <label>Calle:</label>
+                  <input type="text" class="form-control" id="street" name="street">
+                </div>
+                <div class="col">
+                  <label>Número interior:</label>
+                  <input type="text" class="form-control" id="numint" name="numint">
+                </div>
+                <div class="col">
+                  <label>Número exterior:</label>
+                  <input type="text" class="form-control" id="numext" name="numext">
+                </div>
               </div>
-              <div class="col">
-                <label>Celular:</label>
-                <input type="text" class="form-control" id="cellphone" name="cellphone">
+              <div class="row">
+                <div class="col">
+                  <label>Colonia:</label>
+                  <input type="text" class="form-control"  id="colony" name="colony">
+                </div>
+                <div class="col">
+                  <label>Municipio:</label>
+                  <input type="text" class="form-control" id="city" name="city">
+                </div>
+                <div class="col">
+                  <label>Estado:</label>
+                  <input type="text" class="form-control" id="estate" name="estate">
+                </div>
+              </div>
+              <div class="row">
+                <div class="col">
+                  <label>Pais:</label>
+                  <input type="text" class="form-control" id="country" name="country">
+                </div>
+                <div class="col">
+                  <label>Cóigo postal:</label>
+                  <input type="text" class="form-control" id="postalcode" name="postalcode">
+                </div>
+              </div>
+              <div class="row">
+                <div class="col">
+                  <label>Referencia:</label>
+                  <textarea class="form-control" id="reference" name="reference"></textarea>
+                </div>
               </div>
             </div>
-            <div class="row">
-              <div class="col">
-                <label>Calle:</label>
-                <input type="text" class="form-control" id="street" name="street">
-              </div>
-              <div class="col">
-                <label>Número interior:</label>
-                <input type="text" class="form-control" id="numint" name="numint">
-              </div>
-              <div class="col">
-                <label>Número exterior:</label>
-                <input type="text" class="form-control" id="numext" name="numext">
-              </div>
-            </div>
-            <div class="row">
-              <div class="col">
-                <label>Colonia:</label>
-                <input type="text" class="form-control"  id="colony" name="colony">
-              </div>
-              <div class="col">
-                <label>Municipio:</label>
-                <input type="text" class="form-control" id="city" name="city">
-              </div>
-              <div class="col">
-                <label>Estado:</label>
-                <input type="text" class="form-control" id="estate" name="estate">
-              </div>
-            </div>
-            <div class="row">
-              <div class="col">
-                <label>Pais:</label>
-                <input type="text" class="form-control" id="country" name="country">
-              </div>
-              <div class="col">
-                <label>Cóigo postal:</label>
-                <input type="text" class="form-control" id="postalcode" name="postalcode">
-              </div>
-            </div>
-            <div class="row">
-              <div class="col">
-                <label>Referencia:</label>
-                <textarea class="form-control" id="reference" name="reference"></textarea>
-              </div>
-            </div>
-          </div>
             <input class="btn btn-primary" type="submit" value="Registrar">
           </form>
         </div>
