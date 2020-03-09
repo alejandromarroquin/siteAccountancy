df319356aa6c83ceec034295ddd8f30fe7b74c38
Alejandro <alejandro.marroquin@dyctech.com>
2020-03-09 09:13:29 -0600

    Generación y descarga de estado de resultados

--- app/Http/Controllers/ReportsController.php
+++ app/Http/Controllers/ReportsController.php
@@ -113,7 +113,45 @@ class ReportsController extends Controller
 
     //Descarga el estado de resultados
     public function downloadStatementresult(){
-      $pdf = \PDF::loadView('reports.statementresultPDF');
+      $company=User::join('companies','users.idCompany','=','companies.id')->join('taxinformations','companies.idTaxInformation','=','taxinformations.id')->select('taxinformations.businessName')->where('users.id',auth()->user()->id)->get();
+      $accountnames=DB::select('select DISTINCT accountName from accountancycatalogs inner join accountcatalogs on accountancycatalogs.CodeAccount=accountcatalogs.id');
+      $sales=DB::select('select accountcatalogs.accountName,sum(amount) as sumsales from accountancycatalogs inner join accountcatalogs on accountancycatalogs.CodeAccount=accountcatalogs.id inner join accounts on accountcatalogs.idgrouperaccount=accounts.groupcode inner join cashflows on accountancycatalogs.id=cashflows.idaccountancycreditor where accounts.groupcode>400 AND accounts.groupcode<500 GROUP BY accountName');
+      $costs=DB::select('select accountcatalogs.accountName,sum(amount) as sumcosts from accountancycatalogs inner join accountcatalogs on accountancycatalogs.CodeAccount=accountcatalogs.id inner join accounts on accountcatalogs.idgrouperaccount=accounts.groupcode inner join cashflows on accountancycatalogs.id=cashflows.idaccountancydebtor where accounts.groupcode>500 AND accounts.groupcode<600 GROUP BY accountName');
+      $expenses=DB::select('select accountcatalogs.accountName,sum(amount) as sumexpenses from accountancycatalogs inner join accountcatalogs on accountancycatalogs.CodeAccount=accountcatalogs.id inner join accounts on accountcatalogs.idgrouperaccount=accounts.groupcode inner join cashflows on accountancycatalogs.id=cashflows.idaccountancydebtor where accounts.groupcode=602 OR accounts.groupcode=603 OR accounts.groupcode=701 OR accounts.groupcode=703 GROUP BY accountName');
+      $taxes=DB::select('select accountcatalogs.accountName,sum(amount) as sumtaxes from accountancycatalogs inner join accountcatalogs on accountancycatalogs.CodeAccount=accountcatalogs.id inner join accounts on accountcatalogs.idgrouperaccount=accounts.groupcode inner join cashflows on accountancycatalogs.id=cashflows.idaccountancycreditor where accounts.groupcode=602 OR accounts.groupcode=603 OR accounts.groupcode=701 OR accounts.groupcode=703 GROUP BY accountName');
+      $arraysales=array();
+      foreach ($sales as $sale) {
+        $arraysales[$sale->accountName]=$sale->sumsales;
+      }
+      $arraycosts=array();
+      foreach ($costs as $cost) {
+        $arraycosts[$cost->accountName]=$cost->sumcosts;
+      }
+      $arrayexpenses=array();
+      foreach ($expenses as $expense) {
+        $arrayexpenses[$expense->accountName]=$expense->sumexpenses;
+      }
+      $totalsales=0;
+      $totalcosts=0;
+      $grossprofit=0;
+      $totalexpenses=0;
+      foreach ($accountnames as $accountname) {
+        if(array_key_exists($accountname->accountName,$arraysales)){
+          $totalsales=$totalsales+$arraysales[$accountname->accountName];
+        }
+        if(array_key_exists($accountname->accountName,$arraycosts)){
+          $totalcosts=$totalcosts+$arraycosts[$accountname->accountName];
+        }
+        if(array_key_exists($accountname->accountName,$arrayexpenses)){
+          $totalexpenses=$totalexpenses+$arrayexpenses[$accountname->accountName];
+        }
+
+      }
+      $grossprofit=$totalsales-$totalcosts;
+      $utilitybeforetaxes=$grossprofit-$totalexpenses;
+      $incometaxes=$utilitybeforetaxes*.32;
+      $netprofit=$utilitybeforetaxes-$incometaxes;
+      $pdf = \PDF::loadView('reports.statementresultPDF',compact('company','arraysales','arraycosts','arrayexpenses','accountnames','totalsales','totalcosts','grossprofit','totalexpenses','utilitybeforetaxes','incometaxes','netprofit'));
       return $pdf->download();
     }
 
--- resources/views/reports/statementresultPDF.blade.php
+++ resources/views/reports/statementresultPDF.blade.php
@@ -4,10 +4,6 @@
     <meta charset="utf-8">
     <title></title>
     <style media="screen">
-    *{
-      margin: 0px;
-      padding: 0px;
-    }
 
     .col-lg-8{
       width: 100%;
@@ -17,7 +13,9 @@
     }
 
     .margin{
-      width: 90%;
+      margin-left: auto;
+      margin-right: auto;
+      width: 100%;
     }
 
     .information{
@@ -30,67 +28,123 @@
       text-align: center;
     }
 
-    .rows{
-      text-indent: 4em;
-    }
-
     .item{
-      text-indent: 7em;
+      text-align: left;
     }
 
     .import{
+      text-align: right;
+      padding-right: 2em;
+    }
+    table {
+      border-collapse: collapse;
+    }
+
+
+    table, td, th {
+      width: 100%;
+      border-collapse: separate;
+      border-spacing: 10px 5px;
+    }
+
+    .thead{
       text-align: center;
     }
 
-    </style>
+    .theadimport{
+      padding-right: 2em;
+      text-align: right;
+    }
+
+    .count{
+      padding-left: 2em;
+    }
+
+  </style>
   </head>
   <body>
     <div class="row margin">
       <div class="col-lg-8 col-xl-12">
         <div class="container information">
-
+          @foreach($company as $information)
+            <label>{{$information->businessName}}</label><br/>
+          @endforeach
           <label>Estado de resultados al</label>
           <label id="date"></label>
         </div>
-        <table class="table">
-          <thead>
+        <table>
+        <tr>
+          <th class="thead">Rubro</th>
+          <th class="theadimport">Importe</th>
+        </tr>
+        <tr>
+          <td colspan="2"><strong>Ventas</strong></td>
+        </tr>
+        @foreach($accountnames as $accountname)
+          @if(array_key_exists($accountname->accountName,$arraysales))
             <tr>
-              <th scope="col" rowspan="2" class="title">Rubro</th>
-              <th scope="col" rowspan="2" class="title">Importe</th>
+              <td class="count">{{$accountname->accountName}}</td>
+              <td class="import">{{$sale=$arraysales[$accountname->accountName]}}</td>
+
             </tr>
-          </thead>
-          <tbody>
+          @endif
+        @endforeach
+        <tr>
+          <td class="count"><strong>Total de ventas</strong></td>
+          <td class="import"><hr><strong>{{$totalsales}}</strong></td>
+        </tr>
+        <tr>
+          <td colspan="2"><strong>Costo de ventas</strong></td>
+        </tr>
+        @foreach($accountnames as $accountname)
+          @if(array_key_exists($accountname->accountName,$arraycosts))
             <tr>
-              <th scope="row" class="rows">Ventas</th>
+              <td class="count">{{$accountname->accountName}}</td>
+              <td class="import">{{$cost=$arraycosts[$accountname->accountName]}}</td>
+
             </tr>
+          @endif
+        @endforeach
+        <tr>
+          <td class="count"><strong>Total de costos</strong></td>
+          <td class="import"><hr><strong>{{$totalcosts}}</strong></td>
+        </tr>
+        <tr>
+          <td colspan=""><strong>Utilidad bruta</strong></td>
+          <td class="import"><strong>{{$grossprofit}}</strong></td>
+        </tr>
+        <tr>
+          <td colspan="2"><strong>Gastos</strong></td>
+        </tr>
+        @foreach($accountnames as $accountname)
+          @if(array_key_exists($accountname->accountName,$arrayexpenses))
             <tr>
-              <td scope="row" class="item">La</th>
-                <td class="cols import">$4424</td>
+              <td class="count">{{$accountname->accountName}}</td>
+              <td class="import">{{$arrayexpenses[$accountname->accountName]}}</td>
+
             </tr>
-            <tr>
-              <th scope="row" class="rows">Costo de ventas</th>
-              <td class="cols"></td>
-            </tr>
-            <tr>
-              <td scope="row" class="item">La</th>
-                <td class="cols import">$4424</td>
-            </tr>
-            <tr>
-              <th scope="row" class="rows">Gastos</th>
-              <td class="cols"></td>
-            </tr>
-            <tr>
-              <td scope="row" class="item">La</th>
-                <td class="cols import">$4424</td>
-            </tr>
-            <tr>
-              <th scope="row" class="rows">Impuestos</th>
-            </tr>
-            <tr>
-              <td scope="row" class="item">La</th>
-              <td class="cols import">$4424</td>
-            </tr>
-          </tbody>
+          @endif
+        @endforeach
+        <tr>
+          <td class="count"><strong>Total de gastos</strong></td>
+          <td class="import"><strong><hr>{{$totalexpenses}}</strong></td>
+        </tr>
+        <tr>
+          <td class=""><strong></strong></td>
+          <td class="import"><strong></strong></td>
+        </tr>
+        <tr>
+          <td class=""><strong>Total antes de impuestos</strong></td>
+          <td class="import"><strong>{{$utilitybeforetaxes}}</strong></td>
+        </tr>
+        <tr>
+          <td class=""><strong>Impuestos a la utilidad</strong></td>
+          <td class="import"><strong>{{$incometaxes}}</strong></td>
+        </tr>
+        <tr>
+          <td class=""><strong>Utilidad neta</strong></td>
+          <td class="import"><strong>{{$netprofit}}</strong></td>
+        </tr>
         </table>
       </div>
     </div>
